47c5e7d545aa19ea12f8e1580375d97492de08eb
Rez <82224862+Rezthecoder@users.noreply.github.com>
2025-08-05 00:51:44 +0900

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -18,9 +18,7 @@ const Index = () => {
       <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-50 pointer-events-none">
         <LiquidGlass width={300} height={200} />
       </div>
-
       <Header />
-
       <main>
         <Hero />
         <About />
@@ -31,7 +29,6 @@ const Index = () => {
         <News />
         <Contact />
       </main>
-
       <Footer />
     </div>
   );
